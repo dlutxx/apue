e6d899855d674347fd6a871093dd7a086f447378
xuxiang <dlutxx@gmail.com>
2013-08-09 19:17:11 +0800

    signal hellowold

---
+++ signal.chap10/sighello.c
@@ -0,0 +1,34 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef void (*sighand)(int);
+
+static void handle(int sig)
+{
+    if (SIGUSR1 == sig)
+        printf("USR1 received\n");
+    else if (SIGUSR2 ==sig)
+        printf("USR2 received\n");
+    else 
+        exit(0);
+}
+
+int main(int argc, char* argv[])
+{
+    if (SIG_ERR == signal(SIGUSR1, &handle)){
+        perror("signal error");
+        exit(1);
+    }
+
+    if (SIG_ERR == signal(SIGUSR2, &handle)){
+        perror("signal error");
+        exit(2);
+    }
+
+    for(; ;)
+        pause();
+}
+
+
